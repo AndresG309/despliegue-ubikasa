39d401df0d359f8e6e7493a58a30ba2c9ef8f92b
Maria2421 <mp.becerra2424@gmail.com>
2026-05-13 16:53:02 -0500

    a

--- microservicio-propiedades/src/server.ts
+++ microservicio-propiedades/src/server.ts
@@ -1,14 +1,25 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import app from "./app.js";
-import dotenv from 'dotenv';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => {
-
-  console.log(
-    `Microservicio propiedades ejecutándose en puerto ${PORT}`
-  );
-
+dotenv.config({
+  path: path.resolve(__dirname, ".env")
 });
+
+const variablesEntorno = {
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT,
+  SERVICE_PORT: process.env.PORT
+};
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+

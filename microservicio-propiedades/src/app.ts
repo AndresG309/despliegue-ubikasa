30644278e8d180ac7b084047a056f34f1701810e
Maria2421 <mp.becerra2424@gmail.com>
2026-05-13 01:08:47 -0500

    morgan

--- microservicio-propiedades/src/app.ts
+++ microservicio-propiedades/src/app.ts
@@ -2,11 +2,14 @@ import express from "express";
 
 import cors from "cors";
 
+import morgan from 'morgan'; 
+
 import propiedadesRoutes from "./routes/propiedadesRoutes.js";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use(express.json());
 

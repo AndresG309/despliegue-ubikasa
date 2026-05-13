5d17456dc81784722be0dafc79fd073b819dd4d4
Andrés Fernández <andresgabriel309@gmail.com>
2026-05-13 14:26:04 -0500

    Actualizar server.ts

--- microservicio-usuarios/src/server.ts
+++ microservicio-usuarios/src/server.ts
@@ -1,7 +1,14 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config({ path: "././.env" });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, ".env")
+});
 
 const PORT = process.env.PORT || 3001;
 

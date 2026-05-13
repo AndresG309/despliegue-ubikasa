89cb983c4680603d6c9278536872408d91983d65
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-05-13 18:11:29 -0500

    Cambios en dotenv para server y db

--- microservicio-usuarios/src/server.ts
+++ microservicio-usuarios/src/server.ts
@@ -7,15 +7,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({
-  path: path.resolve(__dirname, ".env")
+  path: path.resolve(__dirname, "../.env"),
 });
 
 const variablesEntorno = {
   DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
-  SERVICE_PORT: process.env.PORT
-}
+  SERVICE_PORT: process.env.PORT,
+};
 console.log("Variables de entorno cargadas:", variablesEntorno);
 
 const PORT = process.env.PORT || 3001;
